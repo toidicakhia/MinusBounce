7c63c608fa33f6ffbbdf4d690f44a1db549e389f
toidicakhia <toidicakhia1234@gmail.com>
2023-12-21 16:13:59 +0700

    fix

--- src/main/java/net/minusmc/minusbounce/features/module/modules/combat/velocitys/other/OldLuckyVNVelocity.kt
+++ src/main/java/net/minusmc/minusbounce/features/module/modules/combat/velocitys/other/OldLuckyVNVelocity.kt
@@ -6,7 +6,8 @@ import net.minecraft.network.play.server.S12PacketEntityVelocity
 import net.minusmc.minusbounce.value.FloatValue
 
 class OldLuckyVNVelocity : VelocityMode("OldLuckyVN") {
-    val packet = event.packet
+    override fun onPacket(event: PacketEvent) {
+        val packet = event.packet
         if (packet is S12PacketEntityVelocity) {
             packet.motionX = 0
             packet.motionZ = 0
