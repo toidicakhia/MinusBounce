0ed2dbc1aed1593eb15a279431f2afddb7c2cc50
toidicakhia <toidicakhia1234@gmail.com>
2023-12-24 10:24:21 +0700

    fix

--- src/main/java/net/minusmc/minusbounce/features/module/modules/movement/NoSlow.kt
+++ src/main/java/net/minusmc/minusbounce/features/module/modules/movement/NoSlow.kt
@@ -54,6 +54,7 @@ class NoSlow : Module() {
     val sneakForwardMultiplier = FloatValue("SneakForwardMultiplier", 1.0F, 0.3F, 1.0F, "x")
     val sneakStrafeMultiplier = FloatValue("SneakStrafeMultiplier", 1.0F, 0.3F, 1.0F, "x")
 
+    val noSprintValue = BoolValue("NoSprint", false)
     // Soulsand
     val soulsandValue = BoolValue("Soulsand", true)
     val liquidPushValue = BoolValue("LiquidPush", true)
--- src/main/java/net/minusmc/minusbounce/features/module/modules/movement/Sprint.kt
+++ src/main/java/net/minusmc/minusbounce/features/module/modules/movement/Sprint.kt
@@ -18,29 +18,30 @@ import net.minusmc.minusbounce.utils.Rotation
 import net.minusmc.minusbounce.utils.RotationUtils
 import net.minusmc.minusbounce.value.BoolValue
 import net.minecraft.network.play.client.C0BPacketEntityAction
+import net.minecraft.item.ItemSword
 import net.minecraft.potion.Potion
-import net.minusmc.minusbounce.ui.client.hud.element.elements.targets.impl.Minus
+import net.minecraft.util.MathHelper
+import net.minecraft.util.Vec3
 
 @ModuleInfo(name = "Sprint", description = "Automatically sprints all the time.", category = ModuleCategory.MOVEMENT)
 class Sprint : Module() {
 
-    val sprintUseItemValue = BoolValue("SprintOnUseItem", false)
-    val sprintSwordValue = BoolValue("SprintOnSword", false)
-
     val allDirectionsValue = BoolValue("AllDirections", true)
-    private val noPacketPatchValue = BoolValue("AllDir-NoPacketsPatch", true) { allDirectionsValue.get() }
-    val moveDirPatchValue = BoolValue("AllDir-MoveDirPatch", false) { allDirectionsValue.get() }
-    private val blindnessValue = BoolValue("Blindness", true)
+    val noPacketPatchValue = BoolValue("AllDir-NoPacketsPatch", true, { allDirectionsValue.get() })
+    val moveDirPatchValue = BoolValue("AllDir-MoveDirPatch", false, { allDirectionsValue.get() })
+    val blindnessValue = BoolValue("Blindness", true)
     val foodValue = BoolValue("Food", true)
-
     val checkServerSide = BoolValue("CheckServerSide", false)
     val checkServerSideGround = BoolValue("CheckServerSideOnlyGround", false)
+
+
+    private var modified = false
 
     @EventTarget
     fun onPacket(event: PacketEvent) {
         val packet = event.packet
         if (allDirectionsValue.get() && noPacketPatchValue.get()) {
-            if (packet is C0BPacketEntityAction && (packet.action == C0BPacketEntityAction.Action.STOP_SPRINTING || packet.action == C0BPacketEntityAction.Action.START_SPRINTING)) {
+            if (packet is C0BPacketEntityAction && (packet.getAction() == C0BPacketEntityAction.Action.STOP_SPRINTING || packet.getAction() == C0BPacketEntityAction.Action.START_SPRINTING)) {
                 event.cancelEvent()
             }
         }
@@ -48,25 +49,26 @@ class Sprint : Module() {
 
     @EventTarget
     fun onUpdate(event: UpdateEvent) {
-        val killAura = MinusBounce.moduleManager[KillAura::class.java]!!
-        val noSlow = MinusBounce.moduleManager[NoSlow::class.java]!!
+        val killAura = MinusBounce.moduleManager[KillAura::class.java]!! as KillAura
+        val noSlow = MinusBounce.moduleManager[NoSlow::class.java]!! as NoSlow
 
-        if (!MovementUtils.isMoving || mc.thePlayer.isSneaking
-            || (blindnessValue.get() && mc.thePlayer.isPotionActive(Potion.blindness))
-            || (foodValue.get() && !(mc.thePlayer.foodStats.foodLevel > 6.0F || mc.thePlayer.capabilities.allowFlying))
-            || (checkServerSide.get() && (mc.thePlayer.onGround || !checkServerSideGround.get()) && !allDirectionsValue.get() && RotationUtils.targetRotation != null && RotationUtils.getRotationDifference(
-                Rotation(mc.thePlayer.rotationYaw, mc.thePlayer.rotationPitch)
-            ) > 30F)
-        ) {
+        if (!MovementUtils.isMoving || mc.thePlayer.isSneaking() ||
+                (noSlow.state && noSlow.noSprintValue.get() && noSlow.isSlowing) ||
+                (blindnessValue.get() && mc.thePlayer.isPotionActive(Potion.blindness)) ||
+                (foodValue.get() && !(mc.thePlayer.getFoodStats().getFoodLevel() > 6.0F || mc.thePlayer.capabilities.allowFlying))
+                || (checkServerSide.get() && (mc.thePlayer.onGround || !checkServerSideGround.get())
+                        && !allDirectionsValue.get() && RotationUtils.targetRotation != null &&
+                        RotationUtils.getRotationDifference(Rotation(mc.thePlayer.rotationYaw, mc.thePlayer.rotationPitch)) > 15F) || killAura.target != null && !killAura.keepSprintValue.get()) {
             mc.thePlayer.isSprinting = false
             return
         }
 
-        if (allDirectionsValue.get() || mc.thePlayer.movementInput.moveForward >= 0.8F)
+        if (allDirectionsValue.get() || mc.thePlayer.movementInput.moveForward >= 0.8F) {
             mc.thePlayer.isSprinting = true
+        }
 
         if (allDirectionsValue.get() && moveDirPatchValue.get() && killAura.target == null)
-            RotationUtils.setTargetRot(Rotation(MovementUtils.rawDirection, mc.thePlayer.rotationPitch))
+            RotationUtils.setTargetRot(Rotation(MovementUtils.getRawDirection(mc.thePlayer.rotationYaw), mc.thePlayer.rotationPitch))
     }
 
 }
