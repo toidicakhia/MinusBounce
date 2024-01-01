592cfd79d5dc91b394cbf8a60132cec79c42b8b3
LongAthelstan <121792617+LongAthelstan@users.noreply.github.com>
2024-01-01 10:24:50 +0700

    Update IntaveNoWeb.kt

--- src/main/java/net/minusmc/minusbounce/features/module/modules/movement/nowebs/other/IntaveNoWeb.kt
+++ src/main/java/net/minusmc/minusbounce/features/module/modules/movement/nowebs/other/IntaveNoWeb.kt
@@ -5,5 +5,12 @@ import net.minusmc.minusbounce.value.FloatValue
 import net.minusmc.minusbounce.utils.MovementUtils
 
 class IntaveNoWeb: NoWebMode("Intave") {
-
+    override fun onUpdate() {
+        if (mc.thePlayer.movementInput.moveStrafe == 0.0F && mc.gameSettings.keyBindForward.isKeyDown && mc.thePlayer.isCollidedVertically) {
+            mc.thePlayer.jumpMovementFactor = 0.74F
+        } else {
+            mc.thePlayer.jumpMovementFactor = 0.2F
+            mc.thePlayer.onGround = true
+        }
+    }
 }
